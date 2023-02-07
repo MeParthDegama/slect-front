eb723382afcdd77dd380f611ec1f55147d2d8174
MeParthDegama <pthreeh@outlook.com>
2023-02-07 19:47:53 +0530

    only add

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import FileFooter from "../components/filesFooter";
 import FilesHeader from "../components/filesHeader";
-import FilesList from "../components/filesList";
+import { FileItem, FileItemSpace } from "../components/filesList";
+import API from "../conf/api";
+import { useAppSelector } from "../state/hooks";
 
 const HomePage = () => {
 
-    let empty: Boolean = false;
+    let token = useAppSelector(s => s.token.token)
+    let [fileList, setFileList] = useState([{ name: "!~!~", isdir: false }])
+    let [fileIsLoad, setFileIsLoad] = useState(true)
+
+    const loadFiles = () => {
+        setFileIsLoad(true)
+        API.post(
+            "/files/", {
+            token: token
+        }).then(r => {
+
+            if (r.data.status) {
+                setFileList(r.data.filelist)
+                setFileIsLoad(false)
+            } else {
+
+            }
+
+        }).catch(e => {
+
+        })
+    }
+
+    // file load effect lock
+    let fileLoadLock = useRef(false)
+    let tmp1 = false
+    useEffect(() => {
+        if (!fileLoadLock.current) {
+            fileLoadLock.current = true
+            return
+        }
+        loadFiles()
+    }, [])
 
     return (
         <div className="files-con">
             <FilesHeader />
             <div className="files-con-main">
                 {(() => {
-                    if (empty) {
+                    if (fileIsLoad) {
+                        return (
+                            <div className="loading-dir">
+                                <div>
+                                    <div className="spinner"></div>
+                                </div>
+                            </div>
+                        )
+                    }
+                    if (fileList.length === 1 && fileList[0]["name"] === "!~!~") {
                         return (
                             <div className="empty-dir">
                                 <div>
@@ -23,7 +66,22 @@ const HomePage = () => {
                             </div>
                         )
                     }
-                    return <FilesList />
+                    return (
+                        <div className="files-list">
+                            {fileList.map(e => {
+                                if (!e["name"].startsWith(".")) {
+                                    return <FileItem name={e["name"]} icon={e["isdir"] ? "folder" : "file"} />
+                                } return null
+                            })}
+                            {(() => {
+                                let fsa: JSX.Element[] = [];
+                                for (let i = 0; i < 15; i++) {
+                                    fsa.push(<FileItemSpace />);
+                                }
+                                return fsa;
+                            })()}
+                        </div>
+                    )
                 })()}
             </div>
             <FileFooter />
